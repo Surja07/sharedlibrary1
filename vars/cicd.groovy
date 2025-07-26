6187694f0116c5d5e8bee52de743a282e7103f59
Surja07 <153250264+Surja07@users.noreply.github.com>
2025-07-26 12:24:46 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,5 +1,4 @@
-
-def gitdownload(repo) 
+def gitdownload(String repo) 
 {
-    git branch: 'main', url: "https://github.com/Surja07/Maven1/${repo}.git"
+    git branch: 'main', url: "https://github.com/Surja07/${repo}.git"
 }
