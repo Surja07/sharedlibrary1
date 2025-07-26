6c95fc9d9ce3429549767c22cfe98a6985949104
Surja07 <153250264+Surja07@users.noreply.github.com>
2025-07-26 20:23:38 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,8 +12,9 @@ def deploymentToSlave1(jobname, ip) {
     sh "scp /var/lib/jenkins/workspace/${jobname}/HelloWorld.java ubuntu@${ip}:/var/lib/tomcat10/webapps/"
 }
 
-def test (jobname) 
-{
+def test (jobname) {
+    stage('4. Compile and Run Java') {
+        echo 'Compiling and running HelloWorld.java...'
         sh """
             javac /var/lib/jenkins/workspace/${jobname}/HelloWorld.java
             java -cp /var/lib/jenkins/workspace/${jobname} HelloWorld > /var/lib/jenkins/workspace/${jobname}/test_output.txt
